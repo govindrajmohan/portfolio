ce7a2fad9a77042f6e12b9ac86eb2a4752da3045
unknown <govindmohan44@yahoo.com>
2023-05-14 21:58:09 +0500

    build created

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Header/>
       <Routes>
       
-          <Route path="/" element={<Home />} />
+          <Route exact path="/portfolio" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/todo" element={<TodoApp />} />
           <Route path="/netflix" element={<Netflix />}/>
